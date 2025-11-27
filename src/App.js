9a8acedbd16910172e4899386ae5f8ed9fc36291
Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2025-11-27 22:36:03 +0700

    section-2.2: basic useState with 1 parameter

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import './App.css';
 import { useState } from 'react';
 // Chỉ tăng  biến count nhưng không render lại giao diện
 function App() {
-  const  [count, setCount] = useState(0);
-  const handleIncrease = () => {
-    setCount(count + 1);
-    console.log(count);
+  const  [name, setName] = useState('');
+  const onChangeName = (e) => {
+    setName(e.target.value);
   }
   return (
     <div className="App">
-      {count}
-      <button onClick={() => {
-        setCount(count + 1);
-        console.log(count);
-      }}>Increase</button>
-      <button onClick={handleIncrease}>Increase</button>
+      <h1>Name: {name}</h1>
+      <input 
+        type="text" 
+        value={name}
+        onChange={e => setName(e.target.value)}
+      />
+      <input type="text" onKeyDown={onChangeName} />
     </div>
   );
 }
