4ff15dd9131caa926da2f003e63f9066175f3e49
Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2025-11-27 22:39:30 +0700

    section-2.2: basic useState with multiple parameter

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import { useState } from 'react';
 // Chỉ tăng  biến count nhưng không render lại giao diện
 function App() {
   const  [name, setName] = useState('');
+  const [age, setAge] = useState(18);
   const onChangeName = (e) => {
     setName(e.target.value);
+  }
+  const handleChangeAge = (value, action) => {
+    if(action === 'increase') {
+      setAge(age + value);
+    } else if(action === 'decrease') {
+      setAge(age - value);
+    }
   }
   return (
     <div className="App">
       <h1>Name: {name}</h1>
-      <input 
-        type="text" 
-        value={name}
-        onChange={e => setName(e.target.value)}
-      />
+      <h2>Age: {age}</h2>
       <input type="text" onKeyDown={onChangeName} />
+      <input 
+        type="button" 
+        value={'+'}
+        onClick={() => handleChangeAge(1, 'increase')}
+      />
+      <input 
+        type="button" 
+        value={'-'}
+        onClick={() => handleChangeAge(1, 'decrease')}
+      />
     </div>
   );
 }
