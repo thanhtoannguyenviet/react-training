cabd6210a5896b3e4ace0145e4781bd9e15e7f6b
Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2025-11-27 22:31:34 +0700

    section-2.1: basic useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
-
+import { useState } from 'react';
 // Chỉ tăng  biến count nhưng không render lại giao diện
 function App() {
-  let count = 0;
+  const  [count, setCount] = useState(0);
+  const handleIncrease = () => {
+    setCount(count + 1);
+    console.log(count);
+  }
   return (
     <div className="App">
       {count}
       <button onClick={() => {
-        count += 1;
+        setCount(count + 1);
         console.log(count);
       }}>Increase</button>
+      <button onClick={handleIncrease}>Increase</button>
     </div>
   );
 }
