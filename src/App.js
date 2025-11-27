4c3680e4fe30999704b8fab0b5505c513913a1b0
Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2025-11-27 22:27:14 +0700

    section-2: no-hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
+// Chỉ tăng  biến count nhưng không render lại giao diện
 function App() {
+  let count = 0;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {count}
+      <button onClick={() => {
+        count += 1;
+        console.log(count);
+      }}>Increase</button>
     </div>
   );
 }
