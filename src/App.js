44ad975c2df0c68614d8108540e7b0139fbfdf7d
Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2025-11-30 23:27:00 +0700

    section-3.2:  fetch  api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, use } from 'react';
 import { clear } from '@testing-library/user-event/dist/clear';
 
 function App() {
-  const [timer, setTimer] = useState(10);
-  
+  const [firms, setFirms] = useState([]);
   useEffect(() => {
-    
-    console.log(timer);
-    if(timer < 0) {
-      return;
-    }
-  
-    const timerId = setInterval(() => {
-      setTimer(prevTimer => prevTimer - 1);
-    } , 1000);
+    fetch('https://api.imdbapi.dev/titles')
+      .then(response => response.json())
+      .then(data => {
+        setFirms(data.titles);
+      })
+      .catch(error => console.error('Error fetching firms:', error));
+    console.log(firms);
+  }, [firms]);
 
-    return () => clearInterval(timerId); // cleanup function
-  }, [timer]); // [timer] = chạy khi timer thay đổi
-  
   return (
     <div>
-      {timer >= 0 ? <h1>Countdown: {timer} seconds</h1> : <h1>Time's up!</h1>}
+      <h1>Firms List</h1>
+      <ul>
+        {firms.map(firm => (
+          <li key={firm.id}>{firm.originalTitle}</li>
+        ))}
+      </ul>
     </div>
   );
 }
