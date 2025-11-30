765a19c9553e724eb6930edd79323e02c7a58e6a
Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2025-11-30 22:20:59 +0700

    section-3.1: basic useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  
+  useEffect(() => {
+    console.log('Count đã thay đổi:', count);
+    // Chạy mỗi khi count thay đổi
+  }, [count]); // [count] = chạy khi count thay đổi
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button onClick={() => setCount(count + 1)}>
+      Clicked {count} times
+    </button>
   );
 }
 
