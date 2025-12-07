e620739b1f6794162fa0a400a95a7a8a2f6bd027
Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2025-12-08 00:41:39 +0700

    section-4.2:  formik with validate

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,26 +1,69 @@
  import { useFormik } from 'formik';
+ const validate = values => {
+   const errors = {};
+   if (!values.firstName) {
+     errors.firstName = 'Required';
+   } else if (values.firstName.length > 5) {
+     errors.firstName = 'Must be 5 characters or less';
+   }
+ 
+   if (!values.lastName) {
+     errors.lastName = 'Required';
+   } else if (values.lastName.length > 20) {
+     errors.lastName = 'Must be 20 characters or less';
+   }
+ 
+   if (!values.email) {
+     errors.email = 'Required';
+   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+     errors.email = 'Invalid email address';
+   }
+ 
+   return errors;
+ };
 const SignupForm = () => {
     const formik = useFormik({
-        initialValues: {
-        email: '',
-        },
-        onSubmit: values => {
-        alert(JSON.stringify(values, null, 2));
-        },
-    });
-    // handleSubmit: A submission handler
-    // handleChange: A change handler to pass to each <input>, <select>, or <textarea>
-    // values: Our form’s current values
+     initialValues: {
+       firstName: '',
+       lastName: '',
+       email: '',
+     },
+     validate,
+     onSubmit: values => {
+       alert(JSON.stringify(values, null, 2));
+     },
+   });
    return (
      <form onSubmit={formik.handleSubmit}>
+       <label htmlFor="firstName">First Name</label>
+       <input
+         id="firstName"
+         name="firstName"
+         type="text"
+         onChange={formik.handleChange}
+         value={formik.values.firstName}
+       />
+       {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
+ 
+       <label htmlFor="lastName">Last Name</label>
+       <input
+         id="lastName"
+         name="lastName"
+         type="text"
+         onChange={formik.handleChange}
+         value={formik.values.lastName}
+       />
+       {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
+ 
        <label htmlFor="email">Email Address</label>
        <input
          id="email"
          name="email"
-         type="text"
+         type="email"
          onChange={formik.handleChange}
          value={formik.values.email}
        />
+       {formik.errors.email ? <div>{formik.errors.email}</div> : null}
  
        <button type="submit">Submit</button>
      </form>
