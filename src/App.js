719cd6aa19215aca266e70c7698c0deca6c86826
Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2025-12-09 22:31:55 +0700

    section-5.1:  basic routes

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     <BrowserRouter>
     <nav>
         <Link to="/">Home</Link> | 
-        <Link to="/about">About</Link> | 
-        <Link to="/contact">Contact</Link>
+        <Link to="/signup">Sign Up</Link> | 
+        <Link to="/admin">Contact</Link>
       </nav>
     <Routes>
       <Route path="/signup" element={<SignupForm />} />
