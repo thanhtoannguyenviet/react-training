ee30a798ee0cbcc2e3001fe15ddcc9470da51ab4
Thanh Toan <thanhtoan.nguyenviet@gmail.com>
2025-12-09 22:15:41 +0700

    section-5.1:  basic routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import SignupForm from './components/SignupForm';
+import SignupForm from './pages/SignupForm';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <SignupForm/>
+    <BrowserRouter>
+    <nav>
+        <Link to="/">Home</Link> | 
+        <Link to="/about">About</Link> | 
+        <Link to="/contact">Contact</Link>
+      </nav>
+    <Routes>
+      <Route path="/signup" element={<SignupForm />} />
+      <Route path="/" element={<HomePage />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
